a2fdaa8fe1be73bca012bafc2f610ee91e59bddd
Justine B <justinegayton@gmail.com>
2019-03-04 17:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file
 
-function myFunction (name) {
-  var name = name.toLowerCase():
-  var drivers = drivers.toLowerCase();
-}
+// function myFunction (name) {
+//   var name = name.toLowerCase():
+//   var drivers = drivers.toLowerCase();
+// }
 
 function findMatching(drivers, name) {
   var drivers = drivers.toLowerCase();
@@ -11,3 +11,22 @@ function findMatching(drivers, name) {
   const result = drivers.filter(driver => lcdriver == lcname)
   return result;
 }
+
+
+// Code your solution in this file
+function findMatching(array, string) {
+  var string = new RegExp(string, 'gi');
+  return array.filter(function(name) { return name.match(string); });
+};
+
+function fuzzyMatch(array, string) {
+  var stringRegEx = new RegExp(string, 'g');
+  return array.filter(function(name) {
+    var newName = name.slice(0, string.length);
+    return newName.match(stringRegEx);
+  });
+};
+
+function matchName(array, string) {
+  return array.filter(function(driver) { return driver.name === string; });
+};
