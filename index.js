4acd58d4fcec673d8cf237b0e1331d56e7770118
Justine B <justinegayton@gmail.com>
2019-03-04 17:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(name) {
   const result = drivers.filter(drivers == name);
