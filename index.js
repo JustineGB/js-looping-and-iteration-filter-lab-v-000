c8b15404d63e29c90d4700b0eb73e2e366001ab5
Justine B <justinegayton@gmail.com>
2019-03-04 17:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function findMatching(drivers, name) {
   const result = drivers.filter(driver => driver == name)
   return result;
 }
+
+function findMatching (driver, string) {
+  return driver.filter(function (dName) {
+    return dName.toLowerCase() === string.toLowerCase();
+  });
+}
+
+// function fuzzyMatch(drivers, string) {
+//   return drivers.filter(driver => driver.slice(0, string.length) === string)
+// }
+
+// function matchName (drivers, name) {
+//   return drivers.filter(driver => driver.name === name)
+// }
