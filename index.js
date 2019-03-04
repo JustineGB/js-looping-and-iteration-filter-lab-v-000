0513b01442df023f17db7942a211f594a61c156e
Justine B <justinegayton@gmail.com>
2019-03-04 17:32:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file
 
-
+function myFunction (name) {
+  var name = name.toLowerCase():
+  var drivers = drivers.toLowerCase();
+}
 function findMatching(drivers, name) {
   var lcdrivers = drivers.toLowerCase();
   var lcname = name.toLowerCase();
