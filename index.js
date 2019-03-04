19862262dbd6e9651f61011ed4ae1d91cc343d81
Justine B <justinegayton@gmail.com>
2019-03-04 17:37:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 // Code your solution in this file
 
-// function myFunction (name) {
-//   var name = name.toLowerCase():
-//   var drivers = drivers.toLowerCase();
-// }
-
 function findMatching(drivers, name) {
-  var name = new RegExp
-  const result = drivers.filter(driver => lcdriver == lcname)
-  return result;
-}
-
-function findMatching(array, string) {
-  var string = new RegExp(string, 'gi');
+  var name = new RegExp(drivers, 'gi');
   return array.filter(function(name) { return name.match(string); });
 };
 
-function fuzzyMatch(array, string) {
-  var stringRegEx = new RegExp(string, 'g');
-  return array.filter(function(name) {
-    var newName = name.slice(0, string.length);
-    return newName.match(stringRegEx);
-  });
-};
+// function fuzzyMatch(array, string) {
+//   var stringRegEx = new RegExp(string, 'g');
+//   return array.filter(function(name) {
+//     var newName = name.slice(0, string.length);
+//     return newName.match(stringRegEx);
+//   });
+// };
 
-function matchName(array, string) {
-  return array.filter(function(driver) { return driver.name === string; });
-};
+// function matchName(array, string) {
+//   return array.filter(function(driver) { return driver.name === string; });
+// };
