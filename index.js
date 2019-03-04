206d82c87b3464a7408a0afdb8163e8dd1c21935
Justine B <justinegayton@gmail.com>
2019-03-04 17:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 
 function findMatching(drivers, name) {
-  const name1 = name.toLowerCase(name)
-  const drivers1 = drivers.toLowerCase(drivers)
   const result = drivers1.filter(driver => driver == name1)
   return result;
 }
