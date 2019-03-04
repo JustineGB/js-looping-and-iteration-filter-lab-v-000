931e3b672f677dad96999d466dc0460927a9c0d4
Justine B <justinegayton@gmail.com>
2019-03-04 17:38:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  var name = new RegExp(drivers, 'gi');
-  return drivers.filter(function(name) { return name.match(name); });
-};
-
-// function fuzzyMatch(array, string) {
-//   var stringRegEx = new RegExp(string, 'g');
-//   return array.filter(function(name) {
-//     var newName = name.slice(0, string.length);
-//     return newName.match(stringRegEx);
-//   });
-// };
-
-// function matchName(array, string) {
-//   return array.filter(function(driver) { return driver.name === string; });
-// };
+  var drivers = drivers.toLowerCase();
+  var name = name.toLowerCase();
+  const result = drivers.filter(driver => driver == name)
+  return result;
+}
